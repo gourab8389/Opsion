8619b72772c0090983688042a332d1cd702c292d
gourab8389 <deyg6988@gmail.com>
2025-08-17 00:04:39 +0530

    Refactor routing and components: add TicketDetailsPage, Login, and SignUp; update routes in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,51 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CheckAuth from "./components/check-auth";
+import TicketsPage from "./pages/tickets";
+import TicketDetailsPage from "./pages/ticket";
+import Login from "./pages/login";
+import Admin from "./pages/admin";
+import SignUp from "./pages/signup";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-    <Routes>
-      <Route 
-      path="/" 
-      element={<Home />} 
-      />
-    </Routes>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <CheckAuth protected={true}>
+              <TicketsPage />
+            </CheckAuth>
+          }
+        />
+        <Route
+          path="/tickets/:id"
+          element={
+            <CheckAuth protected={true}>
+              <TicketDetailsPage />
+            </CheckAuth>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <CheckAuth protected={true}>
+              <Login />
+            </CheckAuth>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <CheckAuth protected={true}>
+              <SignUp />
+            </CheckAuth>
+          }
+        />
+      </Routes>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
--- src/pages/ticket.jsx
+++ src/pages/ticket.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const TicketPage = () => {
+const TicketDetailsPage = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const TicketPage = () => {
   )
 }
 
-export default TicketPage
+export default TicketDetailsPage
