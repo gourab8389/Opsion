7de078a70b75ee399ceefe6eb5871561abaf7f75
gourab8389 <deyg6988@gmail.com>
2025-08-16 23:57:47 +0530

    Add routing and new page components
    - Added react-router-dom dependency for routing.
    - Created CheckAuth, Admin, Login, SignUp, TicketPage, and TicketsPage components.
    - Updated main.jsx to include routing setup.

---
+++ src/components/check-auth.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CheckAuth = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CheckAuth
---
+++ src/pages/tickets.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TicketsPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TicketsPage
